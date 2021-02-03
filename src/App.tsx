e6075b205674eed123d57ce39fd342608ea66984
William <william.helmrath@live.com>
2021-02-03 18:11:04 -0500

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    fetch("https://api.lifx.com/v1/lights/all/effects/breathe", {
+      method: "POST",
+      headers: {"Content-Type": "application/json", "Authorization": `Bearer ${process.env.REACT_APP_LIFX_KEY}`},
+      body: JSON.stringify({
+        color: "green",
+        persist: false,
+        power_on: true
+      })
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Mess with my light</button>
     </div>
   );
 }
