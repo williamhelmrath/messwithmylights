0c6576ecba7656d2a3dc85add13dc801a5419722
William <william.helmrath@live.com>
2021-02-03 20:04:38 -0500

    added auth + rocket league button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { Button, TextField, Box } from "@material-ui/core";
 
-function App() {
+const App: React.FC = () => {
+  const [password, setPassword] = useState<string>("");
 
   const handleClick = () => {
     fetch("https://api.lifx.com/v1/lights/all/effects/breathe", {
       method: "POST",
-      headers: {"Content-Type": "application/json", "Authorization": `Bearer ${process.env.REACT_APP_LIFX_KEY}`},
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_LIFX_KEY}`,
+      },
       body: JSON.stringify({
         color: "green",
         persist: false,
-        power_on: true
-      })
-    })
-  }
+        power_on: false,
+      }),
+    });
+  };
+
+  const handleRocketLeague = () => {
+    fetch("https://api.lifx.com/v1/lights/all/effects/pulse", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_LIFX_KEY}`,
+      },
+      body: JSON.stringify({
+        color: "#ff8a0c",
+        from_color: "#0db2ff",
+        period: 1.5,
+        cycles: 2,
+        persist: false,
+        power_on: false,
+      }),
+    });
+  };
+
+  const handleChange = (e: {
+    currentTarget: { value: React.SetStateAction<string> };
+  }) => setPassword(e.currentTarget.value);
+
   return (
-    <div className="App">
-      <button onClick={handleClick}>Mess with my light</button>
-    </div>
+    <Layout>
+      {password !== "william" ? (
+        <TextField
+          value={password}
+          label="Password"
+          variant="outlined"
+          onChange={handleChange}
+        />
+      ) : (
+        <>
+          <Button onClick={handleClick}>Mess with my light</Button>
+          <Button onClick={handleRocketLeague}>Rocket League Time!</Button>
+        </>
+      )}
+    </Layout>
   );
+};
+interface LayoutProps {
+  children: JSX.Element;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <Box
+    width="100vw"
+    height="70vh"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+  >
+    {children}
+  </Box>
+);
 
 export default App;
